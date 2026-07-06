2ca8b8cce062657284bd6bcf83e908ca4038500a
nancyvanderjagt <nancy@sparklegoat.com>
2026-07-06 14:50:37 -0400

    no-cache for admin pages

--- app/auth/require-login.php
+++ app/auth/require-login.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
+header('Expires: 0');
+
 require_once __DIR__ . '/../config/database.php';
 
 if (session_status() !== PHP_SESSION_ACTIVE) {
